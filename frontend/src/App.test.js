515213c7eac6fb15d2733ba734d920b4f0c6e98f
Damian Evans <damian@bluesky.eu.com>
2020-01-11 20:18:07 +0000

    Added another test to App.test.js demo of running tests by connecting to running container

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,3 +7,9 @@ test('renders learn react link', () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders finbarr saunders', () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/Fnarr fnarr\. Snibbet/i);
+  expect(linkElement).toBeInTheDocument();
+});
