946922f0ae5e2a6c21254b27a68efe815d498248
Damian Evans <damian@bluesky.eu.com>
2020-04-06 23:16:09 +0100

    tests still failing #2

--- src/Biffa.js
+++ src/Biffa.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
 function Biffa() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
       <p>Are yers callin' us a heemasex like?</p>
-      <p>06/04/2020 @23:15</p>
+      <p>06/04/2020 @23:20</p>
       </header>
     </div>
   );
